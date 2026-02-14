a37aaf0fcfaef6ce8869134eda16d3a0dff52267
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-02-14 17:05:26 +0000

    feat: updated user details for user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    # optional username
+    username = models.CharField(
+        max_length=150,
+        blank=True,
+        null=True,
+        unique=True
+    )
+
+    # email
+    email = models.EmailField(unique=True)
+    is_email_verified = models.BooleanField(default=False)
+
+    # firstname & lastname
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+
+    GENDER_CHOICES = [
+        ("M", "Male"),
+        ("F", "Female"),
+        ("O", "Other"),
+    ]
+    gender = models.CharField(
+        max_length=1,
+        choices=GENDER_CHOICES,
+        blank=True,
+        null=True
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def save(self, *args, **kwargs):
+        # auto-generate username from email if empty
+        if not self.username and self.email:
+            base = self.email.split("@")[0]
+            self.username = base
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.email
