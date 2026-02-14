3bd8609d54701ef711013a32e66aba3ee9a3c6b9
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-02-14 13:55:59 +0000

    feat: defined goals model

--- backend/goals/models.py
+++ backend/goals/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings # to link to User model
 
-# Create your models here.
+class Goals(models.Model):
+    # link each goal to a user
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='goals'
+    )
+
+    # system-generated title & description
+    title = models.CharField(max_length=255)
+    description = models.CharField(blank=True)
+
+    # smartified input from AI layer
+    raw_input = models.TextField(blank=True)
+
+    # optional due date
+    due_date = models.DateField(null=True, blank=True)
+
+    # completion status
+    completion_status = models.BooleanField(default=False)
+    
+    # timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.TimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
