70ad9f2c4111e525e40ab8b046af1bc6ca7a0730
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-02-15 12:20:41 +0000

    registered goals app with goals admin

--- backend/goals/admin.py
+++ backend/goals/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Goal
 
-# Register your models here.
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 
+        'user', 
+        'due_date', 
+        'is_completed', 
+        'created_at'
+    )
+
+    list_filter = (
+        'is_completed', 
+        'due_date'
+    )
+
+    search_fields = (
+        'title', 
+        'description', 
+        'user__email'
+    )
