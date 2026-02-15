25eb7e624e56537db228a7e4fbf5a0df8b4cef6f
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-02-15 12:10:17 +0000

    registered users app with admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,74 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
+
+    list_display = (
+        'email', 
+        'username', 
+        'first_name', 
+        'last_name', 
+        'is_staff', 
+        'is_superuser',
+        'is_active'
+    )
+
+    list_filter = (
+        'is_staff', 
+        'is_superuser', 
+        'is_active', 
+        'gender'
+    )
+
+    search_fields = (
+        'email', 
+        'username', 
+        'first_name', 
+        'last_name'
+    )
+
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+
+        ('Personal info', {
+            'fields': (
+                'first_name', 
+                'last_name', 
+                'username', 
+                'gender'
+            )
+        }),
+
+        ('Permissions', {
+            'fields': (
+                'is_active', 
+                'is_staff', 
+                'is_superuser', 
+                'groups', 
+                'user_permissions'
+            )
+        }),
+
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email', 
+                'password1', 
+                'password2',
+                'is_staff',
+                'is_superuser',
+                'is_active'
+            ),
+        }),
+    )
