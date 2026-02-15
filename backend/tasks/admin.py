19b51c5bd39a8794057df8928d6b2a14808b06ed
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-02-15 12:30:40 +0000

    registered tasks app with tasks admin

--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 
+        'goal', 
+        'is_completed', 
+        'created_at'
+    )
+
+    list_filter = ('is_completed',)
+
+    search_fields = (
+        'title', 
+        'description', 
+        'goal__title'
+    )
