c2f45233ce3b6cd07fd9fb024ccaeaa63e45adeb
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-02-25 12:49:33 +0000

    added button to trigger mobile sidebar

--- frontend/src/app/(dashboard)/layout.tsx
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Sidebar, MobileSidebar } from "@/components/custom/sidebar";
+import { Button } from "@/components/ui/button";
+import { ListIcon } from "@phosphor-icons/react/dist/ssr";
 import { useCallback, useState } from "react";
 
 export default function DashboardLayout({
@@ -8,16 +10,24 @@ export default function DashboardLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-    const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(false);
-    const onClose = useCallback(() => setIsMobileSidebarOpen(false), []);
+  const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(false);
+  const onClose = useCallback(() => setIsMobileSidebarOpen(false), []);
 
   return (
     <section className="flex h-dvh">
       <Sidebar />
       <MobileSidebar isOpen={isMobileSidebarOpen} onClose={onClose} />
-      <section className="flex flex-col w-full">
-        <header></header>
-        <aside>{children}</aside>
+      <section className="w-full">
+        <aside>
+          <Button
+            variant="ghost"
+            onClick={() => setIsMobileSidebarOpen(!isMobileSidebarOpen)}
+          >
+            <ListIcon size={20} />
+          </Button>
+
+          {children}
+        </aside>
       </section>
     </section>
   );
