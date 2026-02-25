9a99eed21ad259aff84e56ee24d8c72f07985e99
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-02-25 02:44:22 +0000

    added mobile sidebar props to control its visibility

--- frontend/src/app/(dashboard)/layout.tsx
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import { Sidebar, MobileSidebar } from "@/components/custom/sidebar";
+import { useCallback, useState } from "react";
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+    const [isMobileSidebarOpen, setIsMobileSidebarOpen] = useState(false);
+    const onClose = useCallback(() => setIsMobileSidebarOpen(false), []);
+
   return (
     <section className="flex h-dvh">
       <Sidebar />
-      <MobileSidebar />
+      <MobileSidebar isOpen={isMobileSidebarOpen} onClose={onClose} />
       <section className="flex flex-col w-full">
         <header></header>
         <aside>{children}</aside>
