b1bad23ae677e0c3911e3a8793eb431ab6419ae6
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-02-25 02:33:29 +0000

    feat: added sidebar and mobile sidebar

--- frontend/src/app/(dashboard)/layout.tsx
+++ frontend/src/app/(dashboard)/layout.tsx
@@ -1,1 +1,20 @@
-export default function DashboardLayout() {}
+"use client";
+
+import { Sidebar, MobileSidebar } from "@/components/custom/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <section className="flex h-dvh">
+      <Sidebar />
+      <MobileSidebar />
+      <section className="flex flex-col w-full">
+        <header></header>
+        <aside>{children}</aside>
+      </section>
+    </section>
+  );
+}
