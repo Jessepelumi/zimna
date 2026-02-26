9757758e11f6315a6f518c667fb0afc206d4fc6c
Jesse Adesina <jesseoluwapelumi01@gmail.com>
2026-02-26 14:21:58 +0000

    objectives page

---
+++ frontend/src/app/(dashboard)/objectives/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { SectionHeaderButton } from "@/components/custom/sectionHeader";
+
+export default function Objectives() {
+  return (
+    <div>
+      <section>
+        <SectionHeaderButton text="Overview" onClick={() => {}} />
+      </section>
+
+      <section>
+        <SectionHeaderButton text="Objectives" onClick={() => {}} />
+      </section>
+    </div>
+  );
+}
